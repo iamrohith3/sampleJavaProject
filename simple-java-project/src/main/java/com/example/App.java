f0c9566965b0f6bb9c58c44311bbc0d99d6f9348
iamrohith <iamrohith.3@gmail.com>
2025-06-02 10:25:05 +0530

    Initial Commit

---
+++ simple-java-project/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args){
+        System.out.println("Hello, Jenkins anfd Github!");
+    }
+
+    public int add(int a, int b) {
+        retrun a + b;
+    }
+}
